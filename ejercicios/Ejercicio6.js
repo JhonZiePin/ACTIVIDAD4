4d3cf43e21991e8f184ae444b23f51ed11427b58
JhonZiePin <jsierr51@estudiante.ibero.edu.co>
2025-04-18 14:44:47 -0500

    Actulizacion para prueba

--- ejercicios/Ejercicio6.js
+++ ejercicios/Ejercicio6.js
@@ -1,4 +1,5 @@
 let num=parseInt(prompt("Ingrese la tabla de multiplicar que desea calcular"))
 for(let i=1;i<=10;i++){
     console.log(`${num}x${i}=${num*i}`)
+    window.alert(`${num}x${i}=${num*i}`)
 }
