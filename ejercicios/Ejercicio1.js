43d151cb158ecfd3651133a6e9dc04e07591c792
JhonZiePin <jsierr51@estudiante.ibero.edu.co>
2025-04-18 10:30:49 -0500

    Funciona, pero no se repite

---
+++ ejercicios/Ejercicio1.js
@@ -0,0 +1,11 @@
+let numero;
+let cuadrado;
+
+numero=parseFloat(prompt("Digite un numero"));
+
+if(numero>=0){
+    cuadrado=numero*numero;
+    alert("El cuadrado de "+numero+"es"+cuadrado)
+} else{
+    alert("Se introdujo un numero negativo, fin del programa")
+}
