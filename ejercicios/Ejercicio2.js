2168d468a84290edb5af3c390faa27caff757c66
JhonZiePin <jsierr51@estudiante.ibero.edu.co>
2025-04-18 18:37:07 -0500

    Se corrige para ingresar el numero N que pide la profesora. By Alejo Sierra

--- ejercicios/Ejercicio2.js
+++ ejercicios/Ejercicio2.js
@@ -1,23 +1,23 @@
 let N;
 let numero;
 
-N = parseInt(prompt("Ingresa el número secreto"));
+N = parseInt(prompt("Ingresa el número N"));
 
 function adivinar() {
 numero = parseInt(prompt("Adivina el número"));
 
   if (numero < N) {
-    alert("El número secreto es mayor");
+    alert("El número N es mayor");
     adivinar();
   }
 
   if (numero > N) {
-    alert("El número secreto es menor");
+    alert("El número N es menor");
     adivinar();
   }
 
   if (numero === N) {
-    alert("¡Correcto! Adivinaste el número.");
+    alert("¡Muy Bien! Adivinaste el número.");
   }
 }
 
