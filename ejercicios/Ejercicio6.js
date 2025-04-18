70fb67545bad14622a7b264fbd1bf391bcb80130
JhonZiePin <jsierr51@estudiante.ibero.edu.co>
2025-04-18 18:34:01 -0500

    El ejercicio ya quedo, agregue que se repitiera. By; Jhon Sierra

--- ejercicios/Ejercicio6.js
+++ ejercicios/Ejercicio6.js
@@ -2,18 +2,25 @@ let num;
 let i = 1;
 let resultado;
 
+function pedirNumero() {
+
 num=parseInt(prompt("Ingrese la tabla de multiplicar que desea calcular"));
 
 if (num>=0 && num<=10) {
-  
-  window.alert(`Tabla del ${num} :\n`);
+
+    resultado = `Tabla del ${num}:\n`;
 
   while (i <= 10) {
-    resultado= num + " x " + i + " = " + (num * i) + "\n";
+
+    resultado += `${num} x ${i} = ${num*i}\n`;
     i++;
   }
 
   alert(resultado);
 } else {
-  windoww.alert("Número inválido. Debe estar entre 0 y 10.");
+  window.alert("Número inválido. Debe estar entre 0 y 10.");
+  pedirNumero();
 }
+
+}
+pedirNumero();
