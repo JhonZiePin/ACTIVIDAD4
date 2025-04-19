a56142ea132e3e3e852b6c097f9a9c9644321861
JhonZiePin <jsierr51@estudiante.ibero.edu.co>
2025-04-18 19:54:28 -0500

    El ejercicio 5 ya me esta funcionando como le comente, creo que no se le haria mas. By Jhon Sierra

---
+++ ejercicios/Ejercicio5.js
@@ -0,0 +1,37 @@
+let numPositivos = 0;
+let numNegativos = 0;
+let ceros = 0;
+let sumaPositivos = 0;
+let sumaNegativos = 0;
+let mediaPositivos;
+let mediaNegativos;
+
+for (let i = 1; i <= 10; i++) {
+  let numero = parseInt(prompt(`Ingresa el número a calcular: ${i}:`));
+
+  if (numero > 0) {
+    sumaPositivos += numero;
+    numPositivos++;
+  } else if (numero < 0) {
+    sumaNegativos += numero;
+    numNegativos++;
+  } else {
+    ceros++;
+  }
+}
+
+if (numPositivos > 0) {
+  mediaPositivos = sumaPositivos / numPositivos;
+  window.alert(`La media de los números positivos es: ${mediaPositivos}`);
+} else {
+  window.alert("No se han ingresado números positivos.");
+}
+
+if (numNegativos > 0) {
+  mediaNegativos = sumaNegativos / numNegativos;
+  window.alert(`La media de los números negativos es: ${mediaNegativos}`);
+} else {
+  window.alert("No se han ingresado números negativos.");
+}
+
+window.alert(`La cantidad de ceros es: ${ceros}`);
