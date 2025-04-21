751d302bc394a178214bfcaa47f06058dbcd6665
JhonZiePin <jsierr51@estudiante.ibero.edu.co>
2025-04-21 18:53:09 -0500

    Se actualiza codigo usando Do While como lo explico la profesora. By Jhon Sierra

--- ejercicios/Ejercicio1.js
+++ ejercicios/Ejercicio1.js
@@ -1,16 +1,14 @@
-
-function leerYCalcular() {
 let numero;
 let cuadrado;
 
+do{
 numero=parseFloat(prompt("Digite un numero"));
 
 if(numero>=0){
     cuadrado=numero*numero;
-    window.alert(`El cuadrado de: ${numero} es ${cuadrado}`)
-    leerYCalcular();
+    window.alert(`El cuadrado de: ${numero} es ${cuadrado}`);
 } else{
     window.alert("Se introdujo un numero negativo, fin del programa")
 }
 }
-leerYCalcular();
+while (numero >= 0);
